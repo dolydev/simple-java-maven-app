b33ed47ca10e4c0e7102aa21cae23ae7256512d8
dolydev <115890261+dolydev@users.noreply.github.com>
2024-03-14 14:58:51 +0100

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+opackage com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello World!error", app.getMessage());
     }
 }
